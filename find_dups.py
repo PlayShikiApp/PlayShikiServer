0c959557410735f3d96ec1117e1c476e0052217e
Shilin Victor <chrono.monochrome@gmail.com>
2019-07-01 15:08:11 +0300

    Optimize dups script for large queries

--- find_dups.py
+++ find_dups.py
@@ -1,32 +1,43 @@
 from sqlalchemy import func
 
 from shikimori import app, models
-from parsers import ongoings
+from parsers import ongoings, get_animes_ids
 
 DEBUG = False
 DRY_RUN = False
 
-ongoings.main()
-anime_ids = ongoings.ONGOING_IDS
+#ongoings.main()
+anime_ids = get_animes_ids.get_animes_ids()
 
-all_videos = models.AnimeVideo.query.filter(models.AnimeVideo.anime_id.in_(anime_ids)).all()
-res = dict()
-dups = []
-for v in all_videos:
-	if v.url in res:
-		dups.append(v.id)
-		continue
-	res[v.url] = v
+def find_dups(anime_ids):
+	all_videos = models.AnimeVideo.query.filter(models.AnimeVideo.anime_id.in_(anime_ids)).all()
+	res = set()
+	dups = set()
+	for v in all_videos:
+		if v.url in res:
+			dups.add(v.id)
+			continue
+		res.add(v.url)
 
-print(dups)
+	print(dups)
 
-if DEBUG:
-	for id in dups:
-		url = models.AnimeVideo.query.filter(models.AnimeVideo.id == id).first().url
-		#print("%d: %d" % (id, app.db.session.query(func.count(models.AnimeVideo.url)).filter(models.AnimeVideo.url == url).scalar()) )
-		others = [v.id for v in models.AnimeVideo.query.filter(models.AnimeVideo.url == url).all() if not v.id == id]
-		print("%d: %s" % (id, str(others)))
+	if DEBUG:
+		for id in list(dups):
+			url = models.AnimeVideo.query.filter(models.AnimeVideo.id == id).first().url
+			#print("%d: %d" % (id, app.db.session.query(func.count(models.AnimeVideo.url)).filter(models.AnimeVideo.url == url).scalar()) )
+			others = [v.id for v in models.AnimeVideo.query.filter(models.AnimeVideo.url == url).all() if not v.id == id]
+			print("%d: %s" % (id, str(others)))
 
-if not DRY_RUN:
-	models.AnimeVideo.query.filter(models.AnimeVideo.id.in_(dups)).delete(synchronize_session=False)
-	app.db.session.commit()
+	if not DRY_RUN:
+		models.AnimeVideo.query.filter(models.AnimeVideo.id.in_(list(dups))).delete(synchronize_session=False)
+		app.db.session.commit()
+
+
+if __name__ == "__main__":
+	total = len(anime_ids)
+	buffer_len = 500
+	#up_to = ((total // buffer_len) + (1 if (total % buffer_len) else 0)) * buffer_len
+	up_to = (total // buffer_len) * buffer_len
+	for i in range(0, up_to, buffer_len):
+		print("%d-%d" % (i, i + buffer_len))
+		find_dups(anime_ids[i: i + buffer_len])
