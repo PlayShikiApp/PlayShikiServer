8fe09e3eb2a1c025f894bb5e9784eb1ffee69950
Shilin Victor <chrono.monochrome@gmail.com>
2020-06-25 10:34:50 +0200

    Add misc DB

---
+++ shikimori/misc.py
@@ -0,0 +1,3 @@
+DATABASE = {
+	38414: {'duration': 1, 'anime_russian': 'Re:Zero. Жизнь с нуля в альтернативном мире: Замороженные узы', 'anime_english': 'Re:Zero kara Hajimeru Isekai Seikatsu - Hyouketsu no Kizuna'}
+}
